03d5c0b5d07de5e2442dd61f19051c567d2a67dd
Andreas Würl <andi@tryb.de>
2025-04-15 18:43:44 +0200

    update

--- src/service/app/raster/model.rs
+++ src/service/app/raster/model.rs
@@ -1,4 +1,36 @@
-pub struct RasterParameters {}
+pub struct GlobalRasterQuery {
+    pub interval_duration: u16,
+    pub interval_offset: u16,
+    pub raster_baselength: u16,
+    pub count_threshold: u8,
+}
+
+pub struct RegionRasterQuery {
+    pub interval_duration: u16,
+    pub interval_offset: u16,
+    pub raster_baselength: u16,
+    pub region: u8,
+    pub count_threshold: u8,
+}
+
+pub struct LocalRasterQuery {
+    pub interval_duration: u16,
+    pub interval_offset: u16,
+    pub raster_baselength: u16,
+    pub longitude_reference: u16,
+    pub latitude_reference: u16,
+    pub count_threshold: u8,
+}
+
+pub struct RasterParameters {
+    pub longitude_start: f64,
+    pub longitude_delta: f64,
+    pub longitude_bins: u16,
+    pub latitude_start: f64,
+    pub latitude_delta: f64,
+    pub latitude_bins: u16,
+
+}
 
 pub struct RasterEntry {
     pub x: i16,
